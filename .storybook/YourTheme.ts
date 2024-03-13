7c3c007e9685304087982fd5319ee20929a2b4b2
asllanmakaj7 <animakaj7@gmail.com>
2024-03-13 13:40:43 +0100

    update

--- .storybook/YourTheme.ts
+++ .storybook/YourTheme.ts
@@ -4,6 +4,6 @@ export default create({
     base: 'light',
     brandTitle: "FluentUI.Slider.PCF",
     brandUrl: "https://github.com/drivardxrm/FluentUI.Slider.PCF",
-   brandImage: "./Shko Online 92x32.svg",
+    brandImage: "./Shko Online 92x32.svg",
     brandTarget: '_self',
 });
