975741d5a03d3c2d518f5492bc936027798396e4
asllanmakaj7 <animakaj7@gmail.com>
2024-03-13 12:08:53 +0100

    Update to Storybook 8

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: "FluentUI.Slider.PCF",
+    brandUrl: "https://github.com/drivardxrm/FluentUI.Slider.PCF",
+   brandImage: "./Shko Online 92x32.svg",
+    brandTarget: '_self',
+});
